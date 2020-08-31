0c88e8dc402384b517b75f1da42ebbcb240a1475
Jure Zbontar <jure.zbontar@gmail.com>
2020-08-31 08:08:26 -0700

    test bn

--- byol/pth_main.py
+++ byol/pth_main.py
@@ -21,7 +21,6 @@ class TestBYOL(unittest.TestCase):
     def test_linear(self):
         def _forward(inputs):
             return hk.Linear(output_size=4, with_bias=True)(inputs)
-
         forward = hk.without_apply_rng(hk.transform_with_state(_forward))
         k = random.PRNGKey(0)
         inputs = random.normal(k, (2, 3))
@@ -36,3 +35,23 @@ class TestBYOL(unittest.TestCase):
         tout = m(j2t(inputs))
 
         assert allclose(jout, tout)
+
+    def test_bn(self):
+        def _forward(inputs, is_training):
+            bn_config = {'decay_rate': 0.9, 'eps': 1e-05, 'create_scale': True, 'create_offset': True}
+            return hk.BatchNorm(**bn_config)(inputs, is_training=is_training)
+        forward = hk.without_apply_rng(hk.transform_with_state(_forward))
+        k = random.PRNGKey(0)
+        x = [random.normal(kk, (4, 3)) for kk in random.split(k, 3)]
+
+        params, state0 = forward.init(k, x[0], True)
+        jout1, state1 = forward.apply(params, state0, x[1], True)
+        jout2, state2 = forward.apply(params, state1, x[2], False)
+
+        m = nn.BatchNorm1d(3).cuda()
+        tout1 = m.forward(j2t(x[1]))
+        tout2 = m.eval().forward(j2t(x[2]))
+
+        assert allclose(jout1, tout1)
+        assert allclose(jout2, tout2)
+
