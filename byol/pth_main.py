241f915f5b53e5aaae7ec44ab8e087f7a813d89b
Jure Zbontar <jure.zbontar@gmail.com>
2020-09-02 00:34:52 -0700

    mlp works

--- byol/pth_main.py
+++ byol/pth_main.py
@@ -67,4 +67,48 @@ class TestBYOL(unittest.TestCase):
         assert allclose(jout3, tout3, atol=1e-7)
 
     def test_mlp(self):
-        pass
+        def _forward(inputs, is_training):
+            bn_config = {'decay_rate': 0.9, 'eps': 1e-05, 'create_scale': True, 'create_offset': True}
+            return networks.MLP('predictor', 8, 4, bn_config)(inputs, is_training=is_training)
+        forward = hk.without_apply_rng(hk.transform_with_state(_forward))
+        k = random.PRNGKey(0)
+        x = random.normal(k, (4, 3))
+        params, state = forward.init(k, x, True)
+        jout, _ = forward.apply(params, state, x, True)
+
+        sd = sd_j2t(convert_mlp(params, state))
+        m = MLP(3, 8, 4).cuda()
+        m.load_state_dict(sd)
+        tout = m(j2t(x))
+
+        assert allclose(jout, tout)
+
+def add_prefix(prefix, params):
+    return {f'{prefix}.{k}': v for k, v in params.items()}
+
+def sd_j2t(sd):
+    return {k: j2t(v) for k, v in sd.items() if v is not None}
+
+def convert_mlp(params, state):
+    assert len(params.keys()) == 3
+    n = list(params.keys())[0].split('/')[0]
+    return dict(
+        **add_prefix('0', convert_linear(params[f'{n}/linear'])),
+        **add_prefix('1', convert_batchnorm(
+            params[f'{n}/batch_norm'], 
+            state[f'{n}/batch_norm/~/mean_ema'], 
+            state[f'{n}/batch_norm/~/var_ema'])),
+        **add_prefix('3', convert_linear(params[f'{n}/linear_1'])))
+
+def convert_batchnorm(params, mean_ema, var_ema):
+    return dict(weight=params['scale'].ravel(), bias=params['offset'].ravel(), running_mean=mean_ema['hidden'].ravel(), running_var=var_ema['hidden'].ravel())
+
+def convert_linear(params):
+    return dict(weight=params['w'].T, bias=params.get('b'))
+
+def MLP(input_size, hidden_size, output_size):
+    return nn.Sequential(
+        nn.Linear(input_size, hidden_size, bias=True),
+        nn.BatchNorm1d(hidden_size),
+        nn.ReLU(),
+        nn.Linear(hidden_size, output_size, bias=False))
